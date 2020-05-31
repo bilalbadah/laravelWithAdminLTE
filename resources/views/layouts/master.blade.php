c76161704d479f7a7a54a217367e34583a43d368
bilal badah <aymanbilal2@gmail.com>
2020-05-31 17:00:59 +0100

    this commit for video number 7

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -179,7 +179,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item has-treeview ">
-                        <a href="#" class="nav-link active">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-cogs"></i>
                             <p>
                                 Management
@@ -188,7 +188,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
+                                <a href="#" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Active Page</p>
                                 </a>
